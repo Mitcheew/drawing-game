322f940f71b147e317068d078e196b032f28fa01
Mitcheew <wowmormon@gmail.com>
2019-09-25 09:29:49 -0600

    getting things ready and researching how to get this app to work

---
+++ src/components/canvas.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+export default class Canvas extends Component {
+    constructor(props) {
+        super(props);
+
+    }
+    componentDidMount() {
+    }
+    render() {
+        
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
